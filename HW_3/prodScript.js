d02adad835c0157b8960ba866a6fdf36f790d303
Alex-Krasko <terabit.company@gmail.com>
2023-10-08 18:43:49 +0300

    All crashed (

--- HW_3/prodScript.js
+++ HW_3/prodScript.js
@@ -1,63 +1,71 @@
 let data = [];
 let blk = document.querySelector('.blk');
-let dat = [];
-
-
+// let dat = [];
 
 for (let i = 0; i < localStorage.length; i++) {
-    id = i;
     let key = localStorage.key(i);
     let info = localStorage.getItem(key);
 
-    data.push({ id: key, info: info });
+    data.push({ key: key, info: info });
 }
-console.log(data);
+
+// data.forEach(el => {
+//     let temp = el.info.split(':')[0]
+//     if (dat.length !== 0) {
+//         dat.forEach(elem => {
+//             if (elem.includes(temp)) {
+//                 elem.push(el.info.split(':')[1])
+//             }
+//         });
+//     }
+//     else dat.push(el.info.split(':'));
+
+//     console.log(dat);
+
+
+// })
+
+// console.log(data);
 
 data.forEach(el => {
-    let temp = el.info.split(':')[0]
-    console.log(temp);
-    if (!dat.includes(temp)) {
-        dat.push(el.info.split(':'));
-    }
-    else {
-        dat[dat.indexOf(el.info.split(':')[0])] = dat[dat.indexOf(el.info.split(':')[0])] + ',' + el.info.split(':')[1];
-    }
-    console.log(dat.includes(el.info.split(':')[0]));
-    // console.log(dat);
-
-})
-console.log(dat);
-
-dat.forEach(el => {
-    // let dat = (el.info.split(':'));
-
     let div = blk.appendChild(document.createElement('div'))
     let hEl = div.appendChild(document.createElement('h2'));
-    let pEl = div.appendChild(document.createElement('p'));
-    let delPEl = div.appendChild(document.createElement('button'));
-    delPEl.textContent = "Удалить";
-    pEl.style.visibility = 'hidden';
-    delPEl.style.visibility = 'hidden';
-    hEl.textContent = el[0];
-    pEl.textContent = el[1];
+    dat = el.info.split(',');
 
-    // console.log(dat);
+    for (i = 0; i < dat.length; i++) {
+        let dvBlk = div.appendChild(document.createElement('div'));
+        let pEl = dvBlk.appendChild(document.createElement('p'));
+        let delPEl = dvBlk.appendChild(document.createElement('button'));
+        pEl.textContent = dat[i];
+        delPEl.textContent = "Удалить";
+
+        dvBlk.style.visibility = 'hidden';
+        dvBlk.setAttribute('id', 'rev');
+        // console.log(dat[i]);
+    }
+
+    hEl.textContent = el.key;
+
 });
 
-
+console.log(data);
 
 let hProd = document.querySelectorAll('h2');
 hProd.forEach(el => {
     el.addEventListener('click', () => {
-        let p = el.parentElement.querySelector('p');
-        let btn = el.parentElement.querySelector('button');
-        if (p.style.visibility == 'visible') {
-            p.style.visibility = 'hidden ';
-            btn.style.visibility = 'hidden';
+        // let p = el.parentElement.querySelectorAll('#info');
+        let dvBlk = el.parentElement.querySelectorAll('#rev');
+        if (dvBlk[0].style.visibility == 'visible') {
+            dvBlk.forEach(el => {
+                el.style.visibility = 'hidden ';
+            });
+            // btn.style.visibility = 'hidden';
         }
         else {
-            p.style.visibility = 'visible ';
-            btn.style.visibility = 'visible';
+            dvBlk.forEach(el => {
+                el.style.visibility = 'visible ';
+            });
+            // btn.style.visibility = 'visible';
         }
 
     })
@@ -68,16 +76,37 @@ let delReview = document.querySelectorAll('button');
 delReview.forEach(el => {
     el.addEventListener('click', () => {
         let p = el.parentElement.querySelector('p');
-        let h = el.parentElement.querySelector('h2');
-        let btn = el.parentElement.querySelector('button');
+        let h2 = el.parentElement.parentElement.querySelector('h2');
+        let dvBlk = el.parentElement;
+
         data.forEach(elem => {
-            let dat = (elem.info.split(':'));
-            if (dat[1] == p.textContent) {
-                localStorage.removeItem(elem.id)
+            // let dat = elem.key;
+            let datInf = elem.info.split(',');
+
+            if (elem.key == h2.textContent) {
+                // localStorage.removeItem(elem.id)
+                datInf.forEach(el => {
+                    if (el == p.textContent) {
+                        datInf = datInf.filter((el) => {
+                            return el !== p.textContent
+                        })
+
+                    }
+                    return true;
+                });
+
             }
+            if (datInf.length !== 0) {
+                elem.info = datInf;
+                localStorage.setItem(elem.key, datInf)
+            }
+            else localStorage.removeItem(elem.key);
         })
-        el.parentElement.removeChild(p);
-        el.parentElement.removeChild(h);
-        el.parentElement.removeChild(btn);
+        // console.log(el.parentElement.parentNode);
+        if (el.parentElement.hasChildNodes()) {
+            dvBlk.remove();
+        }
+
     })
+
 })
